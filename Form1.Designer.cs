05e6fe0c2d8cd532a940ca1e49e5f5ef2c7317c7
Aldrin Tilos <tilosaldrin@gmail.com>
2022-07-16 09:38:49 +0800

    PDF Resume

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btn_generate = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // btn_generate
             // 
-            this.button1.Location = new System.Drawing.Point(322, 111);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(81, 66);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btn_generate.Location = new System.Drawing.Point(322, 111);
+            this.btn_generate.Name = "btn_generate";
+            this.btn_generate.Size = new System.Drawing.Size(81, 66);
+            this.btn_generate.TabIndex = 0;
+            this.btn_generate.Text = "button1";
+            this.btn_generate.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
@@ -56,7 +56,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_generate);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -66,7 +66,7 @@
 
         #endregion
 
-        private Button button1;
+        private Button btn_generate;
         private Label label1;
     }
 }
