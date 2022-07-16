e5376571680c1e2e661a4eed21552c4eff608550
Aldrin Tilos <tilosaldrin@gmail.com>
2022-07-16 09:46:08 +0800

    Label change name

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.btn_generate = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_generate = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btn_generate
@@ -41,21 +41,21 @@
             this.btn_generate.Text = "button1";
             this.btn_generate.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // lbl_generate
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(303, 62);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 15);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.lbl_generate.AutoSize = true;
+            this.lbl_generate.Location = new System.Drawing.Point(303, 62);
+            this.lbl_generate.Name = "lbl_generate";
+            this.lbl_generate.Size = new System.Drawing.Size(38, 15);
+            this.lbl_generate.TabIndex = 1;
+            this.lbl_generate.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_generate);
             this.Controls.Add(this.btn_generate);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -67,6 +67,6 @@
         #endregion
 
         private Button btn_generate;
-        private Label label1;
+        private Label lbl_generate;
     }
 }
