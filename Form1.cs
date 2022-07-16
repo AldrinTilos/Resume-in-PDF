01451474a75184d23f3b719daa1e431a7433084a
Aldrin Tilos <tilosaldrin@gmail.com>
2022-07-16 10:41:53 +0800

    Namespace and Resume.json file Added

--- Form1.cs
+++ Form1.cs
@@ -1,7 +1,18 @@
+using Newtonsoft.Json;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using System;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Linq;
+
 namespace Resume_PDF
 {
     public partial class Form1 : Form
     {
+        private readonly string _fileName = @"C:\Users\tilos\source\repos\Resume PDF\Resume.json";
         public Form1()
         {
             InitializeComponent();
